1053a50910f96f262a4ea8b2f497de538cb23de3
Organic-Code <lazarelucas@yahoo.fr>
2015-11-05 14:04:44 +0100

    Fixing percentage display for Windows

--- src/client0/getConsoleWidth.cpp
+++ src/client0/getConsoleWidth.cpp
@@ -7,7 +7,7 @@ unsigned int getConsoleWidth(){
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	GetConsoleScreenBufferInfo( GetStdHandle(STD_OUTPUT_HANDLE), &csbi );
 
-	return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	return csbi.srWindow.Right - csbi.srWindow.Left;
 #else
 	
 	struct winsize ws;
