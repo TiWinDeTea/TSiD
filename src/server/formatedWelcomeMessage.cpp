ffb94343bad5e8af693c190748774fff945107df
Organic-Code <lazarelucas@yahoo.fr>
2015-11-16 19:42:26 +0100

    De-warned for realease

--- src/server/formatedWelcomeMessage.cpp
+++ src/server/formatedWelcomeMessage.cpp
@@ -54,7 +54,7 @@ void formatedWelcomeMessage(std::string& message, Client& client){
     			}
     			else{
     				message += ("$[" + keyword + "]");
-    				i += (keyword.size());
+    				i = static_cast<unsigned short>( i + (keyword.size()) );
     			}
     			break;
 
